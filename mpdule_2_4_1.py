5ea5e6ed9d9aad08b06ec3568bec51b0e381070c
KKategor <kamchatkasweethome@gmail.com>
2024-10-11 23:35:41 +0300

    Add files via upload

---
+++ mpdule_2_4_1.py
@@ -0,0 +1,21 @@
+# Lesson Цикл for. Элементы списка. Полезные функции в цикле
+
+numbers = [10, 2, 3, 4, 5, 6, 7, 8, 9, 1, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+n = (len(numbers))
+for i in range(n):
+    if numbers[i] == 1:
+        continue
+    is_prime = True
+    for j in range(2, numbers[i]):
+        if (numbers[i] % j == 0):
+            is_prime = False
+            break
+    if is_prime:
+        primes.extend([numbers[i]])
+    else:
+        not_primes.extend([numbers[i]])
+print('numbers = ',numbers)
+print('Primes = ', primes)
+print('Not_primes = ', not_primes)
